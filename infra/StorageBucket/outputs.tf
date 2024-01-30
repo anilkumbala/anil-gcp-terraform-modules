5621d2207cb51862513bbf2568b87f7576a9315c
Anil Kumbala <HR311@sgpltech.com>
2024-01-30 19:41:33 +0530

    modules modified for storage bucket

--- infra/StorageBucket/outputs.tf
+++ infra/StorageBucket/outputs.tf
@@ -1,32 +1,14 @@
-
-
-
-
-
-
-
-
-output "network" {
-  value       = try(module.networking[0].network, null)
-  description = "The network to which TFE is attached."
+output "bucket" {
+  description = "The created storage bucket"
+  value       = google_storage_bucket.bucket
 }
 
-output "service_account" {
-  value       = module.service_accounts.service_account
-  description = "The service account associated with the TFE instance."
+output "name" {
+  description = "Bucket name."
+  value       = google_storage_bucket.bucket.name
 }
 
-output "subnetwork" {
-  value       = try(module.networking[0].subnetwork, null)
-  description = "The subnetwork to which TFE is attached."
-}
-
-output "dns_configuration_notice" {
-  value       = "If you are using external DNS, please make sure to create a DNS record using the lb_address output that has been provided"
-  description = "A warning message."
-}
-
-output "vm_mig" {
-  value       = module.vm_mig
-  description = "The managed instance group module."
+output "url" {
+  description = "Bucket URL."
+  value       = google_storage_bucket.bucket.url
 }
