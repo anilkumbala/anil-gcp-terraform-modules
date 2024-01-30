00f74ba4072940458059d4d9c2fa618f93ec6942
Anil Kumbala <HR311@sgpltech.com>
2024-01-30 19:46:15 +0530

    modules modified for storage bucket

--- modules/object_storage/outputs.tf
+++ modules/object_storage/outputs.tf
@@ -1,14 +1,4 @@
-output "bucket" {
-  description = "The created storage bucket"
-  value       = google_storage_bucket.bucket
-}
-
 output "name" {
   description = "Bucket name."
   value       = google_storage_bucket.bucket.name
-}
-
-output "url" {
-  description = "Bucket URL."
-  value       = google_storage_bucket.bucket.url
 }
