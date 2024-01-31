13bebe9946f8bcff58c85fe25f97812ede4e28d5
Anil Kumbala <HR311@sgpltech.com>
2024-01-31 18:54:33 +0530

    Modified Modules

--- infra/mig/variables.tf
+++ infra/mig/variables.tf
@@ -48,3 +48,8 @@ variable "service_account" {
   })
   description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template#service_account."
 }
+variable "machine_type" {
+  description = "Machine type to create, e.g. n1-standard-1"
+  type        = string
+  default     = ""
+}
