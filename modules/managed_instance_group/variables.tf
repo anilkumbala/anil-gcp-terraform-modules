5d640baa122297b935cfcc78bf2ee6e31bb91808
Anil Kumbala <HR311@sgpltech.com>
2024-01-31 19:46:41 +0530

    Modified Modules

--- modules/managed_instance_group/variables.tf
+++ modules/managed_instance_group/variables.tf
@@ -260,11 +260,3 @@ variable "mig_timeouts" {
     delete = "15m"
   }
 }
-
-variable "service_account" {
-  type = object({
-    email  = string
-    scopes = set(string)
-  })
-  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template#service_account."
-}
