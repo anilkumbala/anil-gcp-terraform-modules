a87e759bc1688baf4abfd5bdfa2b1464d1976860
Anil Kumbala <HR311@sgpltech.com>
2024-02-02 09:27:45 +0530

    fixing firewall issue for template

--- infra/InstanceTemplate/environment/dev/variables.tfvars
+++ infra/InstanceTemplate/environment/dev/variables.tfvars
@@ -3,7 +3,10 @@ project_id      = "excellent-guide-410011"
 env             = "dev"
 service_account = {
   email  = "940391681103-compute@developer.gserviceaccount.com"
-  scopes = ["https://www.googleapis.com/auth/admin.directory.group"]
+  scopes = ["https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write",
+    "https://www.googleapis.com/auth/devstorage.full_control"]
 }
 subnetwork = "default"
 machine_type = "e2-medium"
--- infra/InstanceTemplate/main.tf
+++ infra/InstanceTemplate/main.tf
@@ -1,3 +1,9 @@
+ terraform {
+  backend "gcs" {
+    bucket = var.bucket # GCS bucket name to store terraform tfstate
+    prefix = var.prefix              # Prefix name should be unique for each Terraform project having same remote state bucket.
+  }
+}
 provider "google" {
 
   project = var.project_id
@@ -5,7 +11,7 @@ provider "google" {
 }
 
 resource "google_compute_address" "ip_address" {
-  name = "${var.env}-external-ip"
+  name = "${var.env}-external-ip-2"
 }
 
 locals {
@@ -21,7 +27,7 @@ module "instance_template" {
   subnetwork                   = var.subnetwork
   stack_type                   = "IPV4_ONLY"
   service_account              = var.service_account
-  name_prefix                  = "${var.env}-instance-template"
+  name_prefix                  = "anil-${var.env}-instance-template"
   machine_type                 = var.machine_type
   tags                         = var.tags
   labels                       = var.labels
